503391b90253dde79c1c852e0990d614b96ef617
xinchi <xinchi.yang@dreampix.cn>
2020-07-17 01:10:43 +0800

    feat: 解决addRoutes爆栈问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { getAuthRoutes } from '@/utils/helper/auth';
 import basicRoutes from './routes/basic';
 import asyncRoutes from './routes/async';
 
@@ -10,9 +11,20 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: basicRoutes,
 });
+let flag = true;
+/* 大坑 条件判断使用addRoutes 不然爆栈 */
+router.beforeEach((to, from, next) => {
+  if (flag) {
+    const accessedRoutes = getAuthRoutes(asyncRoutes, []);
+    router.options.routes = router.options.routes.concat(accessedRoutes);
+    router.addRoutes(accessedRoutes);
+    flag = false;
+    next({ ...to, replace: true });
+  } else {
+    next();
+  }
+});
 
-router.options.routes = router.options.routes.concat(asyncRoutes);
-router.addRoutes(asyncRoutes);
-
+// router.options.routes = router.options.routes.concat(asyncRoutes);
 
 export default router;
